087a843cbc9cc0910071ea7e8e2ea3f23288bef7
RippeR37 <ripper37@gmail.com>
2020-08-10 20:51:11 +0200

    Integrate Google Benchmark framework. Add first performance test (post-tasking).
    This commit integrates added Google Benchmark framework for performance testing
    (with microbenchmarks) and adds first performance test suite which measures
    performance of post-tasking within a single thread and between two different
    threads (albeit in a simple manner, for now).
    
    In future, this will be used to measure performance and track performance
    regressions across commits.

---
+++ tests/perf/base/threading/thread_perftests.cc
@@ -0,0 +1,78 @@
+#include "benchmark/benchmark.h"
+
+#include "base/bind.h"
+#include "base/synchronization/waitable_event.h"
+#include "base/threading/thread.h"
+
+namespace {
+
+void TestSingleThreaded(base::SequencedTaskRunner* tr,
+                        int count,
+                        base::WaitableEvent* event) {
+  if (count > 0) {
+    tr->PostTask(FROM_HERE,
+                 base::BindOnce(&TestSingleThreaded, tr, count - 1, event));
+  } else {
+    event->Signal();
+  }
+}
+
+void TestDoubleThreaded(base::SequencedTaskRunner* tr1,
+                        base::SequencedTaskRunner* tr2,
+                        int count,
+                        base::WaitableEvent* event) {
+  if (count > 0) {
+    tr1->PostTask(FROM_HERE, base::BindOnce(&TestDoubleThreaded, tr2, tr1,
+                                            count - 1, event));
+  } else {
+    event->Signal();
+  }
+}
+
+static void BM_TestSingleThreaded(benchmark::State& state) {
+  base::Thread t1;
+  t1.Start();
+  base::WaitableEvent event{base::WaitableEvent::ResetPolicy::kAutomatic};
+
+  for (auto _ : state) {
+    TestSingleThreaded(t1.TaskRunner().get(), 1000, &event);
+    event.Wait();
+  }
+}
+
+static void BM_TestDoubleThreaded(benchmark::State& state) {
+  base::Thread t1;
+  base::Thread t2;
+  t1.Start();
+  t2.Start();
+  base::WaitableEvent event{base::WaitableEvent::ResetPolicy::kAutomatic};
+
+  for (auto _ : state) {
+    TestDoubleThreaded(t2.TaskRunner().get(), t1.TaskRunner().get(), 1000,
+                       &event);
+    event.Wait();
+  }
+}
+
+#define LIBBASE_BENCHMARK(x)                                                  \
+  BENCHMARK(x)                                                                \
+      ->Unit(::benchmark::TimeUnit::kMicrosecond)                             \
+      ->Repetitions(10)                                                       \
+      ->ComputeStatistics(                                                    \
+          "min",                                                              \
+          [](auto& values) {                                                  \
+            return *(std::min_element(std::begin(values), std::end(values))); \
+          })                                                                  \
+      ->ComputeStatistics(                                                    \
+          "max",                                                              \
+          [](auto& values) {                                                  \
+            return *(std::max_element(std::begin(values), std::end(values))); \
+          })                                                                  \
+      ->DisplayAggregatesOnly()
+
+LIBBASE_BENCHMARK(BM_TestSingleThreaded);
+LIBBASE_BENCHMARK(BM_TestDoubleThreaded);
+
+#undef LIBBASE_BENCHMARK
+
+}  // namespace
