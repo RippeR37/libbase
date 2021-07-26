dbf07944cf10cfc9d63607017bf4e145d4a97a83
RippeR37 <ripper37@gmail.com>
2021-07-26 14:12:52 +0200

    Improved GitHub Actions CI building for Windows and Linux
    This commit adds new configurations to build on Ubuntu and Windows
    within GitHub Actions CI script to improve testing scope.
    
    Furthermore it also sets up clang-format check and code coverage
    using Codecov.io.

--- CMakeModules/SetupCompileFlags.cmake
+++ CMakeModules/SetupCompileFlags.cmake
@@ -13,19 +13,20 @@ function (SETUP_COMPILE_FLAGS)
   endif()
 
   if(LIBBASE_BUILD_TESTS AND LIBBASE_CODE_COVERAGE AND CMAKE_COMPILER_IS_GNUCXX)
-    set(COVERAGE_FLAGS "-g;-fprofile-arcs;-ftest-coverage;-fno-inline;-fno-inline-small-functions;-fno-default-inline;-fno-elide-constructors")
+    set(COVERAGE_COMPILE_FLAGS ";-g;-fprofile-arcs;-ftest-coverage;-fno-inline;-fno-inline-small-functions;-fno-default-inline;-fno-elide-constructors")
+    set(COVERAGE_LINK_FLAGS "-lgcov")
   endif()
 
   if (NOT CONFIGURED_ONCE)
-      set(
-        LIBBASE_FLAGS_PROGRAM
-        "${WARNINGS}"
-        CACHE STRING "Flags used by the compiler during normal build types."
-        FORCE)
-      set(
-        LIBBASE_FLAGS_TESTS
-        "${WARNINGS};${COVERAGE_FLAGS}"
-        CACHE STRING "Flags used by the compiler during test build types."
-        FORCE)
+    set(
+      LIBBASE_COMPILE_FLAGS
+      "${WARNINGS}${COVERAGE_COMPILE_FLAGS}"
+      CACHE STRING "Flags used by the compiler to build targets"
+      FORCE)
+    set(
+      LIBBASE_LINK_FLAGS
+      "${COVERAGE_LINK_FLAGS}"
+      CACHE STRING "Flags used by the linker to link targets"
+      FORCE)
   endif()
 endfunction()
