4cddc2dc491acf83e4cf6d0287a352b0c3ab850f
RippeR37 <ripper37@gmail.com>
2022-04-24 16:35:36 +0200

    fixup! [#24] Update glog version to v0.6.0.

--- src/base/logging.h
+++ src/base/logging.h
@@ -1,3 +1,15 @@
 #pragma once
 
+#ifdef NOMINMAX
+
 #include "glog/logging.h"
+
+#else  // #ifdef NOMINMAX
+
+// Define and undefine `NOMINMAX` to ensure that min()/max() macros are not
+// leaked by glog's implementation by mistake.
+#define NOMINMAX
+#include "glog/logging.h"
+#undef NOMINMAX
+
+#endif  // #ifdef NOMINMAX
