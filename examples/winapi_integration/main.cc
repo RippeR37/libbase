a7e69a34798cc45dd2bad66a4788a6ff21b4f7e1
RippeR37 <ripper37@gmail.com>
2024-02-11 02:18:11 +0100

    [#39] Add new WinApi integration module
    This commit adds a new module (target `libbase_win`) and a class called
    `base::win::WinMessageLoopAttachment` which can be used to integrate
    libbase's cross-thread post-tasking system with Windows native message
    queue system e.g. when building native WinApi applications.
    
    Example usage:
    
    ```cpp
    
    auto loop_attachement = base::win::WinMessageLoopAttachment::Create();
    if (!loop_attachement) {
      // Creation has failed, some error handling here
    }
    
    // As long as the `loop_attachement` lives, current thread will have
    // associated task runner that will post task to Window's message
    // queue and all such tasks will be executed between other messages
    // on that queue.
    
    // Create application window
    HWND hwnd = CreateWindowEx(/* ... */);
    
    // Main loop example
    MSG msg = {};
    while (GetMessage(&msg, NULL, 0, 0) > 0) {
      TranslateMessage(&msg);
      DispatchMessage(&msg);
    }
    ```
    
    To build it, ensure that build option `LIBBASE_BUILD_MODULE_WIN` is
    enabled and then link with `libbase_win` target.
    
    Additionally, a new (Windows-only) example called `winapi_integration`
    has been added to showcase new functionality.

---
+++ examples/winapi_integration/main.cc
@@ -0,0 +1,97 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <thread>
+
+#include "base/message_loop/win/win_message_loop_attachment.h"
+#include "base/platform/windows.h"
+#include "base/threading/thread_pool.h"
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+void SetWindowTitle(HWND hWnd, const wchar_t* title) {
+  SetWindowText(hWnd, title);
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
+  // Setup libbase attachment to main Window thread's message loop
+  auto loopAttachment = base::win::WinMessageLoopAttachment::TryCreate();
+  if (!loopAttachment) {
+    return 0;
+  }
+
+  // Register the window class.
+  const wchar_t CLASS_NAME[] = L"Window Class";
+  WNDCLASS wc = {};
+  wc.lpfnWndProc = &WindowProc;
+  wc.hInstance = hInstance;
+  wc.lpszClassName = CLASS_NAME;
+  RegisterClass(&wc);
+
+  // Adjust window size to account for borders and title bar
+  RECT rect = {0, 0, 800, 600};
+  AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+
+  HWND hwnd =
+      CreateWindowEx(0, CLASS_NAME, L"Default title", WS_OVERLAPPEDWINDOW,
+                     CW_USEDEFAULT, CW_USEDEFAULT, rect.right - rect.left,
+                     rect.bottom - rect.top, NULL, NULL, hInstance, NULL);
+  if (hwnd == NULL) {
+    return 0;
+  }
+
+  ShowWindow(hwnd, nCmdShow);
+
+  // Some example testing
+  loopAttachment->TaskRunner()->PostTask(
+      FROM_HERE, base::BindOnce(&SetWindowTitle, hwnd,
+                                L"Changed window title from post-task"));
+  loopAttachment->TaskRunner()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&SetWindowTitle, hwnd, L"Delayed task executed as well!"),
+      base::Seconds(3));
+
+  base::ThreadPool threadPool{4};
+  threadPool.Start();
+  threadPool.GetTaskRunner()->PostTaskAndReplyWithResult(
+      FROM_HERE, base::BindOnce([]() -> const wchar_t* {
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        return L"Greetings from thread pool!";
+      }),
+      base::BindOnce(&SetWindowTitle, hwnd));
+
+  // Run the message loop.
+  MSG msg = {};
+  while (GetMessage(&msg, NULL, 0, 0) > 0) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+
+  return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd,
+                            UINT uMsg,
+                            WPARAM wParam,
+                            LPARAM lParam) {
+  switch (uMsg) {
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      return 0;
+
+    case WM_CLOSE:
+      DestroyWindow(hwnd);
+      return 0;
+
+    case WM_PAINT: {
+      PAINTSTRUCT ps;
+      HDC hdc = BeginPaint(hwnd, &ps);
+      FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+      EndPaint(hwnd, &ps);
+      return 0;
+    }
+  }
+
+  return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
---
+++ src/base/message_loop/win/win_message_loop_attachment.cc
@@ -0,0 +1,130 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include "base/message_loop/win/win_message_loop_attachment.h"
+
+#include "base/logging.h"
+#include "base/message_loop/message_pump_impl.h"
+#include "base/threading/task_runner_impl.h"
+
+namespace base {
+namespace win {
+
+namespace {
+inline static const UINT WM_LIBBASE_EXECUTE_TASK = WM_USER + 0;
+inline static const wchar_t LIBBASE_CLASS_NAME[] = L"libbaseMessageOnlyWindow";
+
+thread_local WinMessageLoopAttachment* g_current_instance = nullptr;
+
+HWND CreateMessageOnlyWindow(WNDPROC wndProc) {
+  static int static_in_this_module = 0;
+
+  HMODULE hModule = nullptr;
+  GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS |
+                        GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+                    reinterpret_cast<LPCWSTR>(&static_in_this_module),
+                    &hModule);
+
+  WNDCLASS window_class = {};
+  window_class.lpfnWndProc = wndProc;
+  window_class.lpszClassName = LIBBASE_CLASS_NAME;
+  window_class.hInstance = NULL;  // hModule;
+  window_class.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_BTNFACE + 1);
+  RegisterClass(&window_class);
+
+  return CreateWindow(LIBBASE_CLASS_NAME, L"", 0, CW_USEDEFAULT, CW_USEDEFAULT,
+                      CW_USEDEFAULT, CW_USEDEFAULT, HWND_MESSAGE, nullptr,
+                      hModule, nullptr);
+}
+}  // namespace
+
+//
+// WinMessagePumpImpl
+//
+
+namespace detail {
+class WinMessagePumpImpl : public MessagePumpImpl {
+ public:
+  WinMessagePumpImpl(size_t executors_count, HWND hwnd)
+      : MessagePumpImpl(executors_count), hwnd_(hwnd) {}
+
+  // MessagePump
+  bool QueuePendingTask(PendingTask pending_task) override {
+    if (MessagePumpImpl::QueuePendingTask(std::move(pending_task))) {
+      PostMessage(hwnd_, WM_LIBBASE_EXECUTE_TASK, 0, 0);
+      return true;
+    }
+    return false;
+  }
+
+ private:
+  HWND hwnd_;
+};
+}  // namespace detail
+
+//
+// WinMessageLoopAttachment
+//
+
+// static
+std::unique_ptr<WinMessageLoopAttachment>
+WinMessageLoopAttachment::TryCreate() {
+  if (auto hWnd =
+          CreateMessageOnlyWindow(&WinMessageLoopAttachment::WindowProc)) {
+    return std::unique_ptr<WinMessageLoopAttachment>(
+        new WinMessageLoopAttachment(hWnd));
+  }
+
+  return {};
+}
+
+WinMessageLoopAttachment::WinMessageLoopAttachment(HWND hWnd)
+    : hWnd_(hWnd),
+      sequence_id_(base::detail::SequenceIdGenerator::GetNextSequenceId()),
+      message_pump_(std::make_shared<detail::WinMessagePumpImpl>(1, hWnd_)),
+      task_runner_(SingleThreadTaskRunnerImpl::Create(
+          message_pump_,
+          sequence_id_,
+          0,
+          DelayedTaskManagerSharedInstance::GetOrCreateSharedInstance())),
+      scoped_sequence_id_(sequence_id_),
+      scoped_task_runner_handle_(task_runner_) {
+  DCHECK_EQ(g_current_instance, nullptr);
+  g_current_instance = this;
+}
+
+WinMessageLoopAttachment::~WinMessageLoopAttachment() {
+  DCHECK_NE(g_current_instance, nullptr);
+  g_current_instance = nullptr;
+
+  message_pump_->Stop({});
+  DestroyWindow(hWnd_);
+}
+
+std::shared_ptr<SingleThreadTaskRunner> WinMessageLoopAttachment::TaskRunner()
+    const {
+  return task_runner_;
+}
+
+// static
+LRESULT CALLBACK WinMessageLoopAttachment::WindowProc(HWND hWnd,
+                                                      UINT uMsg,
+                                                      WPARAM wParam,
+                                                      LPARAM lParam) {
+  if (uMsg == WM_LIBBASE_EXECUTE_TASK) {
+    DCHECK_NE(g_current_instance, nullptr);
+
+    const MessagePump::ExecutorId executor_id = 0;
+    if (auto pending_task =
+            g_current_instance->message_pump_->GetNextPendingTask(executor_id,
+                                                                  false)) {
+      std::move(pending_task.task).Run();
+    }
+  }
+
+  return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+}  // namespace win
+}  // namespace base
