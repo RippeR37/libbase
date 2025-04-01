RippeR37/libbase

commit c4a9905fa982a2941be6a50c59c4bc67dea0ac5d

[#41] Implement network request cancellation (#45)
This commit adds a functionality to abort/cancel in-flight network
request (on best efforts basis - asynchronously). When a network request
is created, a cancellation token will be return which can be used to try
to cancel the request.

Due to nature of the implementation (asynchronous), the operations is
working on best-effort basis - it may be possible that request's on-done
callback will still be invoked after cancellation of the request.
To ensure it doesn't happen, bind callback to a weak pointer that you
can invalidate when needed. As such, this is should be considered to
mostly being used to avoid wasting resources on a request for which a
response is no longer needed.

examples/simple/main.cc
@@ -157,8 +157,9 @@ void NetExample() {
         for (const auto& [h, v] : response.headers) {
           LOG(INFO) << "  " << h << ": " << v;
         }
-        LOG(INFO) << "Content:\n"
-                  << std::string{response.data.begin(), response.data.end()};
+        LOG_IF(INFO, !response.data.empty())
+            << "Content:\n"
+            << std::string{response.data.begin(), response.data.end()};
         event->Signal();
       },
       &finished_event);

src/base/net/impl/net_thread_impl.cc
@@ -21,6 +21,7 @@ Result CurlCodeToNetResult(CURLcode code) {
 struct NetThread::NetThreadImpl::DownloadInfo {
   ResourceRequest request;
   std::optional<size_t> max_response_size_bytes;
+  RequestCancellationToken cancellation_token;
 
   struct curl_slist* headers = nullptr;
 
@@ -70,6 +71,7 @@ void NetThread::NetThreadImpl::Stop() {
 void NetThread::NetThreadImpl::EnqueueDownload(
     ResourceRequest request,
     std::optional<size_t> max_response_size_bytes,
+    RequestCancellationToken cancellation_token,
     OnceCallback<void(ResourceResponse)> on_done_callback) {
   DCHECK(thread_);
   DCHECK(on_done_callback);
@@ -79,10 +81,24 @@ void NetThread::NetThreadImpl::EnqueueDownload(
     pending_add_downloads_.push_back(DownloadInfo{
         std::move(request),
         std::move(max_response_size_bytes),
+        std::move(cancellation_token),
         nullptr,
         {},
         std::move(on_done_callback),
     });
+  }
+
+  not_modified_.clear();
+  curl_multi_wakeup(multi_handle_);
+}
+
+void NetThread::NetThreadImpl::CancelRequest(
+    RequestCancellationToken cancellation_token) {
+  DCHECK(thread_);
+
+  {
+    std::unique_lock<std::mutex> lock(mutex_);
+    pending_cancel_downloads_.push_back(std::move(cancellation_token));
   }
 
   not_modified_.clear();
@@ -107,11 +123,20 @@ void NetThread::NetThreadImpl::RunLoop_NetThread() {
 void NetThread::NetThreadImpl::ProcessPendingActions_NetThread() {
   if (!not_modified_.test_and_set()) {
     std::unique_lock<std::mutex> lock(mutex_);
+
     for (DownloadInfo& download : pending_add_downloads_) {
       EnqueueDownload_NetThread(download);
     }
     pending_add_downloads_.clear();
-    // TODO: handle pending_cancel_downloads_
+
+    for (RequestCancellationToken cancellation_token :
+         pending_cancel_downloads_) {
+      if (CURL* cancelled_handle =
+              FindHandleByCancellationToken_NetThread(cancellation_token)) {
+        DownloadFinished_NetThread(cancelled_handle, Result::kAborted);
+      }
+    }
+    pending_cancel_downloads_.clear();
   }
 }
 
@@ -183,8 +208,10 @@ void NetThread::NetThreadImpl::EnqueueDownload_NetThread(
   }
 
   // Save download request/response data
-  active_downloads_.insert({easy_handle, std::move(download_info)});
-  const auto& inserted_info = active_downloads_[easy_handle];
+  auto [iter, inserted] =
+      active_downloads_.emplace(easy_handle, std::move(download_info));
+  DCHECK(inserted);
+  const auto& inserted_info = *iter;
 
   // WARNING: Lambda has to be converted to function pointer or it will crash!
   curl_easy_setopt(easy_handle, CURLOPT_WRITEDATA, (void*)(&inserted_info));
@@ -209,15 +236,17 @@ void NetThread::NetThreadImpl::EnqueueDownload_NetThread(
 
 void NetThread::NetThreadImpl::DownloadFinished_NetThread(CURL* finished_curl,
                                                           Result result) {
-  CHECK_GT(active_downloads_.count(finished_curl), 0);
+  auto download_iter = active_downloads_.find(finished_curl);
+  CHECK(download_iter != active_downloads_.end());
 
-  auto& download = active_downloads_[finished_curl];
+  auto& download = download_iter->second;
 
   // Result
   download.response.result = result;
 
   // Response code
-  if (download.response.result == Result::kOk) {
+  if (download.response.result == Result::kOk ||
+      download.response.result == Result::kAborted) {
     long http_code = 0;
     curl_easy_getinfo(finished_curl, CURLINFO_RESPONSE_CODE, &http_code);
     download.response.code = static_cast<int>(http_code);
@@ -272,11 +301,15 @@ void NetThread::NetThreadImpl::DownloadFinished_NetThread(CURL* finished_curl,
 void NetThread::NetThreadImpl::RemoveDownload_NetThread(CURL* finished_curl) {
   // Remove the completed request from multi-handle
   curl_multi_remove_handle(multi_handle_, finished_curl);
-  if (active_downloads_.count(finished_curl) > 0) {
-    if (active_downloads_[finished_curl].headers) {
-      curl_slist_free_all(active_downloads_[finished_curl].headers);
+
+  auto download_iter = active_downloads_.find(finished_curl);
+  if (download_iter != active_downloads_.end()) {
+    auto& download = download_iter->second;
+    if (download.headers) {
+      curl_slist_free_all(download.headers);
     }
   }
+
   curl_easy_cleanup(finished_curl);
 
   // Remove the handle from our list
@@ -296,6 +329,16 @@ void NetThread::NetThreadImpl::AbortAllDownloads_NetThread() {
   }
 
   active_downloads_.clear();
+}
+
+CURL* NetThread::NetThreadImpl::FindHandleByCancellationToken_NetThread(
+    RequestCancellationToken cancellation_token) const {
+  for (const auto& [easy_handle, info] : active_downloads_) {
+    if (info.cancellation_token == cancellation_token) {
+      return easy_handle;
+    }
+  }
+  return nullptr;
 }
 
 }  // namespace net

src/CMakeLists.txt
@@ -65,6 +65,8 @@ target_sources(libbase
     base/net/impl/net_thread_impl.h
     base/net/impl/net_thread.cc
     base/net/impl/net_thread.h
+    base/net/request_cancellation_token.cc
+    base/net/request_cancellation_token.h
     base/net/resource_request.h
     base/net/resource_response.h
     base/net/result.h

src/base/net/impl/net_thread.cc
@@ -1,10 +1,16 @@
 #include "base/net/impl/net_thread.h"
 
+#include <atomic>
+
 #include "base/logging.h"
 #include "base/net/impl/net_thread_impl.h"
 
 namespace base {
 namespace net {
+
+namespace {
+std::atomic_size_t g_request_id_counter_ = 0;
+}
 
 // static
 NetThread& NetThread::GetInstance() {
@@ -26,14 +32,25 @@ void NetThread::Stop() {
   impl_.reset();
 }
 
-void NetThread::EnqueueDownload(
+RequestCancellationToken NetThread::EnqueueDownload(
     ResourceRequest request,
     std::optional<size_t> max_response_size_bytes,
     OnceCallback<void(ResourceResponse)> on_done_callback) {
   DCHECK(impl_);
 
+  RequestCancellationToken cancellation_token{
+      g_request_id_counter_.fetch_add(1)};
+
   impl_->EnqueueDownload(std::move(request), std::move(max_response_size_bytes),
-                         std::move(on_done_callback));
+                         cancellation_token, std::move(on_done_callback));
+
+  return cancellation_token;
+}
+
+void NetThread::CancelRequest(RequestCancellationToken cancellation_token) {
+  DCHECK(impl_);
+
+  impl_->CancelRequest(std::move(cancellation_token));
 }
 
 }  // namespace net

src/base/net/impl/net_thread.h
@@ -4,6 +4,7 @@
 #include <optional>
 
 #include "base/callback.h"
+#include "base/net/request_cancellation_token.h"
 #include "base/net/resource_request.h"
 #include "base/net/resource_response.h"
 
@@ -17,9 +18,12 @@ class NetThread {
   void Start();
   void Stop();
 
-  void EnqueueDownload(ResourceRequest request,
-                       std::optional<size_t> max_response_size_bytes,
-                       OnceCallback<void(ResourceResponse)> on_done_callback);
+  RequestCancellationToken EnqueueDownload(
+      ResourceRequest request,
+      std::optional<size_t> max_response_size_bytes,
+      OnceCallback<void(ResourceResponse)> on_done_callback);
+
+  void CancelRequest(RequestCancellationToken cancellation_token);
 
  private:
   class NetThreadImpl;

src/base/net/impl/net_thread_impl.h
@@ -26,7 +26,10 @@ class NetThread::NetThreadImpl {
 
   void EnqueueDownload(ResourceRequest request,
                        std::optional<size_t> max_response_size_bytes,
+                       RequestCancellationToken cancellation_token,
                        OnceCallback<void(ResourceResponse)> on_done_callback);
+
+  void CancelRequest(RequestCancellationToken cancellation_token);
 
  private:
   struct DownloadInfo;
@@ -40,9 +43,13 @@ class NetThread::NetThreadImpl {
   void Wait_NetThread();
 
   void EnqueueDownload_NetThread(DownloadInfo& download_info);
+  void CancelRequest_NetThread(RequestCancellationToken cancellation_token);
   void DownloadFinished_NetThread(CURL* finished_curl, Result result);
   void RemoveDownload_NetThread(CURL* finished_curl);
   void AbortAllDownloads_NetThread();
+
+  CURL* FindHandleByCancellationToken_NetThread(
+      RequestCancellationToken cancellation_token) const;
 
   // Accessible anywhere
   std::atomic_flag not_quit_;
@@ -51,7 +58,7 @@ class NetThread::NetThreadImpl {
   // Accessible with mutex
   std::mutex mutex_;
   std::vector<DownloadInfo> pending_add_downloads_;
-  // TODO: add pending_cancel_downloads_;
+  std::vector<RequestCancellationToken> pending_cancel_downloads_;
 
   // Accessible on `thread_`
   CURLM* multi_handle_;

src/base/net/request_cancellation_token.cc
@@ -1,0 +1,15 @@
+#include "base/net/request_cancellation_token.h"
+
+namespace base {
+namespace net {
+
+RequestCancellationToken::RequestCancellationToken(size_t request_id)
+    : request_id_(request_id) {}
+
+bool RequestCancellationToken::operator==(
+    const RequestCancellationToken& other) const {
+  return request_id_ == other.request_id_;
+}
+
+}  // namespace net
+}  // namespace base

src/base/net/request_cancellation_token.h
@@ -1,0 +1,18 @@
+#pragma once
+
+#include <cstddef>
+
+namespace base {
+namespace net {
+
+struct RequestCancellationToken {
+  explicit RequestCancellationToken(size_t request_id);
+
+  bool operator==(const RequestCancellationToken& other) const;
+
+ private:
+  size_t request_id_;
+};
+
+}  // namespace net
+}  // namespace base

commit a57820add79a1a902693ac601d92bc1deb8b1bf1

[#41] Implement sending POST requests
This commit adds ability to send data with POST requests.

Additionally it also fixes a memory related bug introduced in PR #45
where wrong pointer was passed to CURL's WriteData function which could
cause crashes.

This also adds an option to CMake to build project with ASAN enabled.

CMakeLists.txt
@@ -46,6 +46,8 @@ else()
   option(LIBBASE_CLANG_TIDY "Build with clang-tidy" OFF)
 endif()
 
+option(LIBBASE_BUILD_ASAN "Build with Address Sanitizer enabled" OFF)
+
 
 #
 # Compiler setup

CMakeModules/SetupCompileFlags.cmake
@@ -50,15 +50,19 @@ function(SETUP_COMPILE_FLAGS)
     set(CLANG_TIDY_PROPERTIES "CXX_CLANG_TIDY;${CLANG_TIDY_EXE}")
   endif()
 
+  if(LIBBASE_BUILD_ASAN)
+    set(SANITIZE_FLAGS ";-fsanitize=address")
+  endif()
+
   if(NOT CONFIGURED_ONCE)
     set(
       LIBBASE_COMPILE_FLAGS
-      "${WARNINGS}${COVERAGE_COMPILE_FLAGS}"
+      "${WARNINGS}${COVERAGE_COMPILE_FLAGS}${SANITIZE_FLAGS}"
       CACHE STRING "Flags used by the compiler to build targets"
       FORCE)
     set(
       LIBBASE_LINK_FLAGS
-      "${COVERAGE_LINK_FLAGS}"
+      "${COVERAGE_LINK_FLAGS}${SANITIZE_FLAGS}"
       CACHE STRING "Flags used by the linker to link targets"
       FORCE)
     set(

examples/simple/main.cc
@@ -140,7 +140,7 @@ void ThreadPoolSingleThreadExample() {
   pool.Stop();
 }
 
-void NetExample() {
+void NetExampleGet() {
   base::Thread thread{};
   thread.Start();
 
@@ -169,9 +169,57 @@ void NetExample() {
       base::net::ResourceRequest{
           "https://www.google.com/robots.txt",
           base::net::kDefaultHeaders,
-          base::net::kNoTimeout,
-          base::net::kNoTimeout,
+          base::net::kNoPost,
           true,
+      },
+      base::BindPostTask(thread.TaskRunner(), std::move(response_callback),
+                         FROM_HERE));
+
+  // Timeout = 5s
+  thread.TaskRunner()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(
+          [](base::WaitableEvent* event) {
+            LOG(WARNING) << "Failed to download in 5 seconds";
+            event->Signal();
+          },
+          &finished_event),
+      base::Seconds(5));
+
+  finished_event.Wait();
+}
+
+void NetExamplePost() {
+  base::Thread thread{};
+  thread.Start();
+
+  base::WaitableEvent finished_event{};
+  auto response_callback = base::BindOnce(
+      [](base::WaitableEvent* event, base::net::ResourceResponse response) {
+        LOG(INFO) << "Result: " << static_cast<int>(response.result);
+        LOG(INFO) << "HTTP code: " << response.code;
+        LOG(INFO) << "Final URL: " << response.final_url;
+        LOG(INFO) << "Downloaded " << response.data.size() << " bytes";
+        LOG(INFO) << "Latency: " << response.timing_connect.InMilliseconds()
+                  << "ms";
+        LOG(INFO) << "Headers";
+        for (const auto& [h, v] : response.headers) {
+          LOG(INFO) << "  " << h << ": " << v;
+        }
+        LOG_IF(INFO, !response.data.empty())
+            << "Content:\n"
+            << std::string{response.data.begin(), response.data.end()};
+        event->Signal();
+      },
+      &finished_event);
+
+  // Try to download and signal on finish
+  const std::string data_str = "{\"key\": \"value\"}";
+  base::net::SimpleUrlLoader::DownloadUnbounded(
+      base::net::ResourceRequest{
+          "https://httpbin.org/post",
+          {"Content-Type: application/json"},
+          std::vector<uint8_t>{data_str.begin(), data_str.end()},
           true,
       },
       base::BindPostTask(thread.TaskRunner(), std::move(response_callback),
@@ -205,7 +253,10 @@ int main(int argc, char* argv[]) {
   ThreadPoolSequencedExample();
   ThreadPoolSingleThreadExample();
 
-  NetExample();
+  LOG(INFO) << "Finished threading examples\n\n\nBeginning networking examples";
+
+  NetExampleGet();
+  NetExamplePost();
 
   LOG(INFO) << "Example finished in " << timer.Elapsed().InMillisecondsF()
             << "ms";

src/base/net/impl/net_thread_impl.cc
@@ -188,9 +188,19 @@ void NetThread::NetThreadImpl::EnqueueDownload_NetThread(
   if (download_info.request.follow_redirects) {
     curl_easy_setopt(easy_handle, CURLOPT_FOLLOWLOCATION, 1L);
   }
-  if (download_info.request.headers_only) {
+
+  // POST or HEAD or GET
+  if (download_info.request.post_data) {
+    curl_easy_setopt(easy_handle, CURLOPT_POST, 1L);
+    curl_easy_setopt(easy_handle, CURLOPT_POSTFIELDS,
+                     download_info.request.post_data->data());
+    curl_easy_setopt(easy_handle, CURLOPT_POSTFIELDSIZE_LARGE,
+                     download_info.request.post_data->size());
+  } else if (download_info.request.headers_only) {
     curl_easy_setopt(easy_handle, CURLOPT_NOBODY, 1L);
   }
+
+  // Headers
   for (const auto& header : download_info.request.headers) {
     download_info.headers =
         curl_slist_append(download_info.headers, header.c_str());
@@ -198,6 +208,8 @@ void NetThread::NetThreadImpl::EnqueueDownload_NetThread(
   if (download_info.headers) {
     curl_easy_setopt(easy_handle, CURLOPT_HTTPHEADER, download_info.headers);
   }
+
+  // Timeouts
   if (!download_info.request.connect_timeout.IsZero()) {
     curl_easy_setopt(easy_handle, CURLOPT_CONNECTTIMEOUT_MS,
                      download_info.request.connect_timeout.InMilliseconds());
@@ -211,7 +223,7 @@ void NetThread::NetThreadImpl::EnqueueDownload_NetThread(
   auto [iter, inserted] =
       active_downloads_.emplace(easy_handle, std::move(download_info));
   DCHECK(inserted);
-  const auto& inserted_info = *iter;
+  const auto& inserted_info = iter->second;
 
   // WARNING: Lambda has to be converted to function pointer or it will crash!
   curl_easy_setopt(easy_handle, CURLOPT_WRITEDATA, (void*)(&inserted_info));
@@ -227,7 +239,7 @@ void NetThread::NetThreadImpl::EnqueueDownload_NetThread(
         }
         info->response.data.insert(info->response.data.end(),
                                    reinterpret_cast<uint8_t*>(data),
-                                   reinterpret_cast<uint8_t*>(data) + n * l);
+                                   reinterpret_cast<uint8_t*>(data) + (n * l));
         return n * l;
       });
 
@@ -292,10 +304,15 @@ void NetThread::NetThreadImpl::DownloadFinished_NetThread(CURL* finished_curl,
     }
   }
 
+  // Save on-done callback for later, but first remove and clean up all CURL
+  // resources to ensure it doesn't use anything provided anymore.
   DCHECK(download.on_done_callback);
-  std::move(download.on_done_callback).Run(std::move(download.response));
+  auto on_done_callback = std::move(download.on_done_callback);
+  auto response = std::move(download.response);
 
   RemoveDownload_NetThread(finished_curl);
+
+  std::move(on_done_callback).Run(std::move(response));
 }
 
 void NetThread::NetThreadImpl::RemoveDownload_NetThread(CURL* finished_curl) {

src/base/net/resource_request.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -9,16 +11,18 @@ namespace base {
 namespace net {
 
 const auto kDefaultHeaders = std::vector<std::string>{};
+const auto kNoPost = std::nullopt;
 const auto kNoTimeout = base::TimeDelta{};
 
 struct ResourceRequest {
   std::string url;
   std::vector<std::string> headers = kDefaultHeaders;
+  std::optional<std::vector<uint8_t>> post_data;
+  bool headers_only = false;
 
   base::TimeDelta connect_timeout = kNoTimeout;
   base::TimeDelta timeout = kNoTimeout;
   bool follow_redirects = true;
-  bool headers_only = false;
 };
 
 }  // namespace net